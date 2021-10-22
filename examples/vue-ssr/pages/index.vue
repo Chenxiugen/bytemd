3689cb6e8548a834fc0ba50688ee2fa7e62ecaa5
chenxiugen <chenxiugen@zuoyebang.com>
2021-10-22 19:05:47 +0800

    feat: 样式调整

--- examples/vue-ssr/pages/index.vue
+++ examples/vue-ssr/pages/index.vue
@@ -1,22 +1,32 @@
 <template>
-  <div>
-    <div style="padding: 10px 0">
-      Plugins:
-      <label v-for="p in Object.keys(enabled)" :key="p">
-        <input
-          type="checkbox"
-          :checked="enabled[p]"
-          @change="handlePluginChange($event, p)"
-        />
-        {{ p }}
-      </label>
+  <div class="editor-container">
+    <div class="editor-header">
+      <div class="plugin-box">
+        Plugins:
+        <label v-for="p in Object.keys(enabled)" :key="p">
+          <input
+            type="checkbox"
+            :checked="enabled[p]"
+            @change="handlePluginChange($event, p)"
+          />
+          {{ p }}
+        </label>
+      </div>
+      <div class="function-btns">
+        <span class="btn" @click="submitGit">提交</span>
+        <span class="btn" @click="saveLocal">保存草稿</span>
+      </div>
     </div>
-    <Editor
-      :value="value"
-      @change="handleChange"
-      :plugins="enabledPlugins"
-      :toolbarItems="[]"
-    />
+
+    <div class="editor-box">
+      <Editor
+        class="editor-content"
+        :value="value"
+        @change="handleChange"
+        :plugins="enabledPlugins"
+        :toolbarItems="[]"
+      />
+    </div>
   </div>
 </template>
 
@@ -25,7 +35,11 @@ import { Editor, Viewer } from '@bytemd/vue'
 import highlight from '@bytemd/plugin-highlight'
 import math from '@bytemd/plugin-math'
 import mermaid from '@bytemd/plugin-mermaid'
+import gfm from '@bytemd/plugin-gfm'
+import gemoji from '@bytemd/plugin-gemoji'
 import { markdownText } from '../test'
+
+const DraftKey = '_MD_DRAFT_'
 
 export default {
   components: {
@@ -34,20 +48,29 @@ export default {
   },
   data() {
     return {
-      value: markdownText,
+      value: '',
       enabled: {
         highlight: true,
         math: true,
         mermaid: true,
+        gemoji: true,
+        GFM: true,
       },
     }
   },
+  mounted() {
+    // 获取草稿信息
+    this.value = this.getLocalDraft()
+  },
   computed: {
+    /** 启用的插件 */
     enabledPlugins() {
       return [
         this.enabled.highlight && highlight(),
         this.enabled.math && math(),
         this.enabled.mermaid && mermaid(),
+        this.enabled.GFM && gfm(),
+        this.enabled.gemoji && gemoji(),
       ]
     },
   },
@@ -57,6 +80,30 @@ export default {
     },
     handlePluginChange($event, p) {
       this.enabled[p] = $event.target.checked
+    },
+    /** 保存草稿 */
+    saveLocal() {
+      sessionStorage &&
+        sessionStorage.setItem(
+          DraftKey,
+          JSON.stringify({
+            data: this.value,
+          })
+        )
+      alert('保存成功')
+    },
+    /** 获取草稿 */
+    getLocalDraft() {
+      let output = markdownText
+      const str = (sessionStorage && sessionStorage.getItem(DraftKey)) || ''
+      try {
+        output = JSON.parse(str).data
+      } catch (error) {}
+      return output
+    },
+    /** 提交git TODO */
+    submitGit() {
+      alert('待实现')
     },
   },
 }
@@ -71,7 +118,43 @@ html {
 body {
   margin: 0 10px;
 }
+.editor-container {
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  padding: 10px;
+}
+.editor-box {
+  position: relative;
+  height: 100%;
+  overflow: hidden;
+}
+.editor-content {
+  height: 100%;
+}
 .bytemd {
-  height: 90vh !important;
+  height: 100% !important;
+}
+
+.editor-header {
+  display: flex;
+  justify-content: space-between;
+  height: 32px;
+  margin-bottom: 10px;
+}
+.function-btns,
+.plugin-box {
+  align-self: center;
+}
+.function-btns .btn {
+  display: inline-block;
+  height: 24px;
+  padding: 0 10px;
+  border-radius: 4px;
+  cursor: pointer;
 }
 </style>
